4b3a9d9b0410019d39df412088c540d83c1d9f47
jupabro <patricia.brosowski@web.de>
2023-10-12 19:20:08 +0200

    UsernamePasswordAuthentificationProvider added in order to adjust whole config

---
+++ src/main/java/com/quiz/config/UsernamePasswordAuthentificationProvider.java
@@ -0,0 +1,54 @@
+package com.quiz.config;
+
+import com.quiz.user.User;
+import com.quiz.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.*;
+
+public class UsernamePasswordAuthentificationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        Optional<User> optionalUser = userRepository.findByEmail(username);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (passwordEncoder.matches(pwd, user.getPassword())) {
+                return new UsernamePasswordAuthenticationToken(username, pwd, getGrantedAuthorities(user.getAuthorities()));
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        }else {
+            throw new BadCredentialsException("Invalid credentials!");
+        }
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Collection<? extends GrantedAuthority> roles) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (GrantedAuthority role : roles) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+        }
+        return grantedAuthorities;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
