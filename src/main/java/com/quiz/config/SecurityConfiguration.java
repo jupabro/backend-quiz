bf4a41c2da35dc6d24a1d824635904bdceef0f35
jupabro <patricia.brosowski@web.de>
2023-10-12 19:27:53 +0200

    cors config added

--- src/main/java/com/quiz/config/SecurityConfiguration.java
+++ src/main/java/com/quiz/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.quiz.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +30,18 @@ public class SecurityConfiguration {  //bind the filter
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-             http
+             http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                         @Override
+                         public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                             CorsConfiguration config = new CorsConfiguration();
+                             config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                             config.setAllowedMethods(Collections.singletonList("*"));
+                             config.setAllowCredentials(true);
+                             config.setAllowedHeaders(Collections.singletonList("*"));
+                             config.setMaxAge(3600L);
+                             return config;
+                         }
+                     }))
                 .csrf(AbstractHttpConfigurer::disable) // disables CSRF (Cross-Site Request Forgery) protection
                         .authorizeHttpRequests((authz) -> authz
                              .requestMatchers("/api/v1/auth/**").permitAll() //whitelist, no auth required
